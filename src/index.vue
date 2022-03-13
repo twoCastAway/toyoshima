b35d8f9522a4f8be37dd78ab58ce406ec9341cb7
tsu <tsukasak4991@gmail.com>
2022-03-14 07:32:52 +0900

    first

---
+++ src/index.vue
@@ -0,0 +1,63 @@
+<template>
+<div>
+    <header>
+        <div id="pageConfig">
+            <ul>
+                <li>文字サイズ・色合い変更</li>
+                <li>ふりがな表示</li>
+                <li>音声読上げ</li>
+            </ul>
+        </div>
+        <div id="siteMap">
+            <ul>
+                <li>サイトマップ</li>
+                <li>携帯サイト</li>
+                <li>アクセス</li>
+            </ul>
+        </div>
+        <div id="selectLanguage">select language</div>
+        <div id="emerjency">防災・緊急重要な情報</div>
+        <div id="callCenter">豊島区役所コールセンター</div>
+    </header>
+
+    <div id="mainWrapper1">
+        <div id="sideMenu">
+            <ul>
+                <li>防災・安全</li>
+                <li>手続き・届出</li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+        <div id="carousel">
+
+        </div>
+        <div id="attention">
+
+        </div>
+
+        <!-- ↓カスタム検索以降 -->
+        <div id="mainWrapper2">
+            <div id="googleCustom">googleCustom</div>
+            <div id="lifeScene">lifeScene</div>
+            <div id="usefulInformation">usefulInformation</div>
+            <div id="wardAdministration">wardAdministration</div>
+            <div id="effort">effort</div>
+            <div id="SNS">sns</div>
+        </div>
+    </div>
+
+    <footer>
+
+    </footer>
+</div>
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import index from './index.vue'
+
+createApp(index).mount('#app')
